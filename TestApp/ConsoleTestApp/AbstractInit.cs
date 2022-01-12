26425f82d0c7094f3527671c1014a1e0729d9161
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2022-01-12 04:42:50 -0800

    TestApp -abstract assignment

---
+++ TestApp/ConsoleTestApp/AbstractInit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestApp
+{
+    internal abstract class AbstractInit
+    {
+        public abstract void Init();
+
+        public abstract void Print();
+        
+    }
+
+    internal class ConcreteInit:AbstractInit
+    {
+        public override void Init()
+        {
+            Console.WriteLine($"Init ...");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"------------- Abstract obj = concrete obj()  Print Method ---------------");
+        }
+    }
+}
---
+++ TestApp/ConsoleTestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleTestApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-------------- ConsoleTest App ----------------");
+
+            AbstractInit abstractInit = new ConcreteInit();
+            abstractInit.Init();
+            abstractInit.Print();
+
+            Console.WriteLine("------------------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
