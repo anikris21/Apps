f897a1abe8af759349a9d92624243c9a95d89b67
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2022-01-12 06:16:53 -0800

    UnitTest App

---
+++ UnitTestApp/UnitTestApp/MathClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestApp
+{
+    public interface IMathClass
+    {
+        int Add(int x, int y);
+
+        int Subtract(int x, int y);
+
+        int Multiply(int x, int y);
+
+        int Divide(int x, int y);
+    }
+
+    internal class MathClass :IMathClass
+    {
+        public int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return x / y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return (x - y);
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ UnitTestApp/UnitTestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UnitTestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("------------ UnitTest App -----------");
+
+            MathClass mathClass = new MathClass();
+
+            Console.WriteLine($"Add 3, 4 {mathClass.Add(3, 4)}");
+            Console.WriteLine($"Subtract 4, 6 {mathClass.Subtract(6, 4)}");
+            Console.WriteLine($"Multiply 4, 6 {mathClass.Multiply(4, 6)}");
+            Console.WriteLine($"Divide   6, 4 {mathClass.Divide(6, 4)}");
+
+            Console.WriteLine("-------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
