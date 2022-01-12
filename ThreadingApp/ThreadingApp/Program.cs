e2440f4b3c96a8fc63df015965cd296f9dd794be
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2022-01-11 23:36:32 -0800

    Thread example

---
+++ ThreadingApp/ThreadingApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using ThreadingApp;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("--------------------------");
+
+        ThreadExec threadExec = new ThreadExec();
+        Thread thread = new Thread(threadExec.thread);
+        thread.Start();
+
+        Console.ReadKey();
+    }
+
+}
---
+++ ThreadingApp/ThreadingApp/ThreadExec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadingApp
+{
+    internal class ThreadExec
+    {
+        public void thread()
+        {
+            Console.WriteLine($"\n-----------Thread start --------------");
+            Console.WriteLine($"managed thread id - {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"exiting thread");
+        }
+    }
+}
