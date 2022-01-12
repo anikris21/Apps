36a38be79c03a73ddd4c49dc386425806808fe10
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2022-01-12 00:18:29 -0800

    Enumarator with inventory

---
+++ EnumeratorApp/EnumeratorApp/Inventory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratorApp
+{
+    internal class Inventory
+    {
+        public string Name;
+        private string Description;
+        public int Price;
+
+
+        public Inventory(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+
+    // foreach support
+    internal class InventoryItems : IEnumerable
+    {
+        private Inventory[] _inventoryItems;
+
+        public InventoryItems(Inventory[] inventories)
+        {
+            _inventoryItems = new Inventory[inventories.Length];
+            for(int i = 0; i < inventories.Length; i++)
+            {
+                _inventoryItems[i] = inventories[i];
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class InventoryItemEnum : IEnumerator
+    {
+        //private Inventory _inventory;
+        private Inventory[] _inventoryItems;
+
+        private int position = -1;
+
+        public InventoryItemEnum(Inventory[] inventories)
+        {
+            _inventoryItems = inventories;
+        }
+
+        public object Current
+        {
+            get
+            {
+                try
+                {
+                    return _inventoryItems[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return position >= _inventoryItems.Length;
+
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ EnumeratorApp/EnumeratorApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using EnumeratorApp;
+
+namespace App
+{
+    public class EnumeratorApp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"--------- EnumeratorApp -------------");
+            Inventory[] inventories = new Inventory[]
+            {
+                new Inventory("PC", 1000),
+                new Inventory("Laptop", 1000),
+                new Inventory("Mobile", 1000)
+            };
+
+            Console.WriteLine($"Enumerated Inventory  - ");
+            foreach (Inventory item in inventories)
+            {
+                Console.WriteLine($" {item.Name}, {item.Price}");
+            }
+
+            Console.WriteLine("--------------------------------------");
+            Console.ReadKey();
+        }
+    } 
+
+
+}
