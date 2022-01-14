b553a75bbe576ab3349a59499276e01707442a2f
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2022-01-14 05:17:30 -0800

    multicast delegate

---
+++ EventDelegateApp/EventApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegateApp
+{
+    public class EventApp
+    {
+        public static void StaticPrint(string s)
+        {
+            Console.WriteLine($" StaticPoint {s}");
+        }
+
+        public void InsPrint(string s)
+        {
+            Console.WriteLine($" InsPoint {s}");
+        }
+    }
+}
---
+++ EventDelegateApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using EventDelegateApp;
+
+namespace App
+{
+    public delegate void Print(string message);
+
+    public class EventDelegateApp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("-------- EventDelgate App ---------------");
+
+            EventApp ob = new EventApp();
+            //ob.InsPrint("Test");
+
+            Print del = ob.InsPrint;
+            del += EventApp.StaticPrint;
+            del += MainPrint;
+
+            del("Delegate call");
+            Console.WriteLine("-----------------------------------------");
+            Console.ReadKey();
+        }
+
+        public static void MainPrint(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
