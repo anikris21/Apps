55deeace787dce36d2f75ff111804956fb0ecb97
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2022-01-16 21:15:54 -0800

    DatabaseApp - creating model/dbContext

---
+++ DatabaseApp/ContosoContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatabaseApp
+{
+    public partial class ContosoContext : DbContext
+    {
+        public ContosoContext()
+        {
+        }
+
+        public ContosoContext(DbContextOptions<ContosoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#pragma warning disable CS1030 // #warning directive
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=localhost\\sqlexpress;Initial Catalog=Contoso;Integrated Security=True;TrustServerCertificate=True");
+#pragma warning restore CS1030 // #warning directive
+                
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.ToTable("Movie");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DatabaseApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace App
+{
+    public class DatabaseApp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("------------ Database App ------------");
+
+
+            Console.WriteLine("--------------------------------------");
+            Console.ReadLine();
+        }
+    }
+}
