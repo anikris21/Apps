0be5c0323a2855615e54a827e5de728fc8b4b39c
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2022-01-20 03:10:40 -0800

    Indexer - generic and specific

---
+++ IndexerApp/PersonCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerApp
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        private Random random = new Random();
+
+        public Person(string name)
+        {
+            Name = name;
+            Id = random.Next(100);
+            Console.WriteLine("Id - ", Id);
+        }
+    }
+
+    internal class PersonCollection<T>
+    {
+        private T[] array = new T[100];
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+    }
+
+    class PersonCollection
+    {
+        private Person[] people1;
+        public PersonCollection(Person[] people)
+        {
+            people1 = new Person[people.Length];
+            for (int i = 0; i < people.Length; i++)
+            {
+                people1[i] = people[i];
+            }
+        }
+
+        public Person this[int index]
+        {
+            get
+            {
+                if(index >= people1.Length)
+                {
+                    throw new IndexOutOfRangeException("Index > initialized index");
+                }
+                return people1[index];
+            }
+            set { people1[index] = value; }
+        }
+    }
+}
---
+++ IndexerApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using IndexerApp;
+
+namespace App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("----------------- IndexerApp -------------------");
+            PersonCollection<Person> collection = new PersonCollection<Person>();
+            collection[0] = new Person("P1");
+            collection[1] = new Person("P11");
+
+            Console.WriteLine(collection[0].Name, collection[0].Id);
+            Console.WriteLine(collection[1].Name, collection[1].Id);
+
+
+            Person[] people = new Person[2];
+            people[0] = new Person("P00");
+            people[1] = new Person("P001");
+
+            PersonCollection personCollection = new PersonCollection(people);
+
+            try
+            {
+                Console.WriteLine(personCollection[2].Name, personCollection[2].Id);
+            } catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(personCollection[0].Name, personCollection[0].Id);
+            Console.WriteLine(personCollection[1].Name, personCollection[1].Id);
+
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------------");
+        }
+    }
+}
