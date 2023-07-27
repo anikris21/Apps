07a478a19cf3362bda3677ba76afddefd7a978b5
Anil Kumar K K <akumarkk@outlook.com>
2023-07-27 20:28:08 +0000

    shopping console app

---
+++ ShoppingApp/ShoppingConsoleApp/BusinessObject/Client.cs
@@ -0,0 +1,27 @@
+﻿// ShppingConsoleApp.
+namespace ShoppingConsoleApp.BusinessObject
+{
+    public class Client
+    {
+        public int Id { get; private init; }
+
+        public string Name { get; private init; }
+
+        public List<string> ShoppingCart { get; private init; }
+
+        public Client(int id, string name)
+        {
+            ShoppingCart = new List<string>();
+            Id = id;
+            Name = name;
+        }
+
+        public void AddToCart(string item)
+        {
+            ShoppingCart.Add(item);
+        }
+
+
+
+    }
+}
---
+++ ShoppingApp/ShoppingConsoleApp/ShoppingClientApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingConsoleApp.BusinessObject;
+
+Client client = new Client(1, "Ace Gardner");
+
+client.AddToCart("carnations");
+client.AddToCart("indigo");
+
+DisplayClient(client);
+void DisplayClient(Client client)
+{
+    Console.WriteLine( "******" + client.Name + "******");
+    foreach(var item in client.ShoppingCart)
+    {
+        Console.WriteLine(item);
+    }
+
+}
