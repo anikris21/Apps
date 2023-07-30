b73e498998e0cbb38bdab8eac14cb178bf6271ee
Anil Kumar K K <akumarkk@outlook.com>
2023-07-30 02:44:23 +0000

    contractors co

---
+++ C10Apps/ContractorsCo/ConsoleDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using Models.Tests.Data;
+
+try
+{
+    foreach (var worker in Workers.TestData)
+    {
+        Console.WriteLine(worker);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Err : {e.Message}");
+}
