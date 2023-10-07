1cb9514dc9a0f595fed32084f1bfcf2061d40c87
Anil Kumar K K <akumarkk@outlook.com>
2023-10-07 23:30:29 +0000

    record and record struct

---
+++ Record/RecordApp/RecordApp/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace RecordApp
+{
+    using System;
+
+    record struct Test
+    {
+        public int Prop { get; set; }
+        public Test(int v)
+        {
+            Prop = v;
+        }
+        public void Print()
+        {
+            Console.WriteLine("*Test1 c");
+        }
+
+        public void Print1()
+        {
+            Console.WriteLine("Test1 virtual  c");
+        }
+    }
+    class Test1
+    {
+        public int Prop { get; set; } = 10;
+        public void Print()
+        {
+            Console.WriteLine("*Test1 c");
+        }
+
+        public virtual void Print1()
+        {
+            Console.WriteLine("Test1 virtual  c");
+        }
+    }
+
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Test1 t = new Test1();
+            Console.WriteLine($"Hash {t.GetHashCode()}");
+            t.Prop = 11;
+            Console.WriteLine($"Hash {t.GetHashCode()}");
+
+            Test t1 = new Test(10);
+            Test t11 = new Test(10);
+            Console.WriteLine(t1 == t11);
+            Console.WriteLine($"struct Hash {t1.GetHashCode()}");
+            t1.Prop = 11;
+            Console.WriteLine($"struct Hash {t1.GetHashCode()}");
+
+            Console.WriteLine("Hello Mono World");
+        }
+    }
+}
