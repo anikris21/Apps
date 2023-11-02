7b75a82691a430fa08196ec8c646dc2fd27c2eda
Anil Kumar K K <akumarkk@outlook.com>
2023-11-02 21:42:41 +0000

    trace app

---
+++ Trace/TraceApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace TraceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Trace.AutoFlush = true;
+            Trace.Indent();
+
+            Trace.WriteLine("Entering main");
+
+
+            Console.WriteLine("Hello, World!");
+
+
+            Trace.WriteLine("Exiting main");
+            Trace.Unindent();
+        }
+    }
+}
