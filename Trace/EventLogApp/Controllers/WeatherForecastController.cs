e4065fec31c5b4319910bbc6ffdf2d3a323b5b41
Anil Kumar K K <akumarkk@outlook.com>
2023-11-03 00:11:33 +0000

    trace app

---
+++ Trace/EventLogApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventLogApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+            //https://stackoverflow.com/questions/74378318/net-logging-level-not-mapping-to-event-viewer-event-level     
+            _logger.LogTrace("weather controller trace entering");
+            _logger.LogError("weather controller trace entering");
+            //_logger.LogInformation("weather controller trace entering");
+
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Trace/EventLogApp/Program.cs
@@ -0,0 +1,34 @@
+namespace EventLogApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddLogging(logbuilder => {
+                logbuilder.AddConsole(options => { options.TimestampFormat = "[HH:mm:ss]"; });
+                logbuilder.AddEventLog(options => { options.LogName = "Application"; });
+            });
+
+            var app = builder.Build();
+
+            
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
