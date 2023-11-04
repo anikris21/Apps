19875eb0bb83b5b356cbca68c1163f3f74d03092
Anil Kumar K K <akumarkk@outlook.com>
2023-11-04 04:43:20 +0000

    framework/windows EWT client

---
+++ Trace/EWTWindowsApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Diagnostics.Tracing;
+//using Microsoft.Diagnostics.Parsers;
+using Microsoft.Diagnostics.Tracing.Session;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETWWindowsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!(TraceEventSession.IsElevated() ?? false))
+            {
+                Console.WriteLine("Run as admin"); 
+                //return;
+
+            }
+
+            TraceEventSession session = new TraceEventSession("TraceApp");
+            session.EnableProvider("TraceAppEventSource", TraceEventLevel.Verbose);
+
+
+            var parser = session.Source.Dynamic;
+            parser.All += e => {
+                for (int i = 0; i < e.PayloadNames.Length; i++) 
+                { 
+                    Console.Write($"{e.PayloadValue(i)} "); 
+                
+                }
+
+                Console.WriteLine();
+            };
+
+            session.Source.Process();
+        }
+    }
+}
