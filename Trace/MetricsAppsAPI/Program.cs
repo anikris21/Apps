c3a0e5792fd72328dd5bbe4b3550c9e26eefe96b
Anil Kumar K K <akumarkk@outlook.com>
2023-11-07 23:20:51 +0000

    Metrics App - Api

---
+++ Trace/MetricsAppsAPI/Program.cs
@@ -0,0 +1,31 @@
+using MetricsAppsAPI.Model;
+using MetricsAppsAPI.Services;
+
+namespace MetricsAppsAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddSingleton<CustomerService>();
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Trace/MetricsAppsAPI/Services/CustomerService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using MetricsAppsAPI.Model;
+namespace MetricsAppsAPI.Services
+{
+    public class CustomerService
+    {
+        int _id = 4;
+        // mock database Id=2,
+        IList<Customer> customerDb = new List<Customer>() { 
+            new Customer { Id=1, FirstName = "Lee", LastName = "Mac", Email = "leemac@metricsapp.com" },
+            new Customer {Id=2, FirstName = "Ram", LastName = "kumar", Email = "ram@metricsapp.com" },
+            new Customer {Id=3, FirstName = "Ravi", LastName = "Pb", Email = "ravi@metricsapp.com" }
+        };
+
+        public Task<Customer> CreateAsync(Customer c1)
+        {
+
+            if (customerDb.Any(c => c.Id == c1.Id)) { 
+                return Task.FromResult<Customer>(null); 
+            }
+            
+            c1.Id = _id++; 
+            customerDb.Add(c1);
+            return Task.FromResult(c1);
+        }
+
+        public Task<Customer?> GetById(int Id)
+        {
+            if (customerDb.Any(c => c.Id == Id))
+                return Task.FromResult(customerDb.First(c1 => c1.Id == Id));
+            return Task.FromResult<Customer>(null);
+        }
+
+        void Test(IList<Customer> list) { }
+
+        // ReadOnlyCollection
+        public Task<ReadOnlyCollection<Customer>> GetAllAsync() {
+            // Test(customerDb.AsReadOnly())
+            return Task.FromResult<ReadOnlyCollection<Customer>>(customerDb.AsReadOnly()); 
+        }
+
+
+
+    }
+}
