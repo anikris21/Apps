f65c2bdbe9a5dcb16c1945a6c03ffdf00f3b2974
Anil Kumar K K <akumarkk@outlook.com>
2023-11-08 00:43:26 +0000

    - App metrics end points - metrics, metrics-text, env. - App metrics middleware for measurements

--- Trace/MetricsAppsAPI/Program.cs
+++ Trace/MetricsAppsAPI/Program.cs
@@ -1,5 +1,7 @@
 using MetricsAppsAPI.Model;
 using MetricsAppsAPI.Services;
+using App.Metrics.Formatters.Prometheus;
+using App.Metrics.AspNetCore.Tracking;
 
 namespace MetricsAppsAPI
 {
@@ -12,11 +14,21 @@ namespace MetricsAppsAPI
             // Add services to the container.
             builder.Services.AddSingleton<CustomerService>();
 
+            builder.Services.AddMetrics();
+            builder.Services.AddMetricsTrackingMiddleware();
+
+            builder.Services.AddMetricsEndpoints();
+
             builder.Services.AddControllers();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            app.UseMetricsAllMiddleware();
+
+            app.UseMetricsAllEndpoints();
+
+            //app.UseMetricsWebTracking();
 
             app.UseHttpsRedirection();
 
