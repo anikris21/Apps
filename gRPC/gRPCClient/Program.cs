de99dfd3bc43794efbb4db8ca26670c9b6e14187
Anil Kumar K K <akumarkk@outlook.com>
2024-04-17 08:27:40 -0700

    gRPC

--- gRPC/gRPCClient/Program.cs
+++ gRPC/gRPCClient/Program.cs
@@ -1,8 +1,19 @@
-﻿namespace Program;
+﻿using Grpc.Net.Client;
+using gRPCService;
+
+namespace Program;
 class Program
 {
-    public static void Main()
+    public static async Task Main()
     {
         Console.WriteLine("Hello, World!");
+        var request = new HelloRequest {Name = "Sunil"};
+        var channel = GrpcChannel.ForAddress("https://localhost:7226");
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(request);
+        // .Message
+        Console.WriteLine(reply.Message);
+        
+
     }
 }
